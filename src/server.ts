5aa5787d167a47d9550eb75f94813243cee54ccb
Suman Shrestha <88821020+NAMUS09@users.noreply.github.com>
2025-01-22 00:40:27 +0545

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://emergency-service-locator.vercel.app"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
